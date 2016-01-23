43f6325c5112cfa511d350ecd67a39215eb47cb6
Samuel Shih <sam00429@gmail.com>
2016-01-22 21:07:10 -0500

    Revert "Idek"
    This reverts commit 1a1d24c71ba5c830c7eccfbc644353133d996223.

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,44 +1,31 @@
 (function() {
-
+ 
   'use strict';
   var express = require('express');
   var router = express.Router();
   var mongojs = require('mongojs');
-  var passport = require('passport');
   var db = mongojs('dare', ['dares']);
-
+ 
   /* GET home page. */
   router.get('/', function(req, res) {
     res.render('index');
   });
-
-  router.get('/login', function(req, res) {
-    res.render('login');
-  });
-
-  router.post('/login',
-    passport.authenticate('local'),
-    function(req, res) {
-      // If this function gets called, authentication was successful.
-      // `req.user` contains the authenticated user.
-      res.redirect('/users/' + req.user.username);
-  });
-
+ 
   router.get('/api/dares', function(req, res) {
     db.dares.find(function(err, data) {
       res.json(data);
     });
   });
-
+ 
   router.post('/api/dares', function(req, res) {
     db.dares.insert(req.body, function(err, data) {
       res.json(data);
     });
-
+ 
   });
-
+ 
   router.put('/api/dares', function(req, res) {
-
+ 
     db.dares.update({
       _id: mongojs.ObjectId(req.body._id)
     }, {
@@ -47,18 +34,18 @@
     }, {}, function(err, data) {
       res.json(data);
     });
-
+ 
   });
-
+ 
   router.delete('/api/dares/:_id', function(req, res) {
     db.dares.remove({
       _id: mongojs.ObjectId(req.params._id)
     }, '', function(err, data) {
       res.json(data);
     });
-
+ 
   });
-
+ 
   module.exports = router;
-
+ 
 }());
